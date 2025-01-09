cac860f2ab87383694a2f3a51a9806d89d5975ab
Jake <jake.j.gerber@gmail.com>
2025-01-08 17:34:32 -0800

    Added ability to upload localstorage cover arts as well as updating links

--- migrateJSON.py
+++ migrateJSON.py
@@ -3,14 +3,62 @@ import boto3
 
 from boto3.dynamodb.conditions import Key, Attr
 
+import os
+
 
 #Load the JSON data
 with open('combined.json', 'r') as file:
     data = json.load(file)
 
+
+#quit()
+
+#WIP for uploading files to the S3 bucket.
+
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('price-test-real')
+
+print(bucket)
+
+for key, game in data.items():
+    try:
+        print("key: ", key)
+        print("game: ", game["cover-link"])
+        file_path = game["cover-link"]
+        _, file_extension = os.path.splitext(file_path)
+        object_name = file_path.split("\\")[-1]
+        bucket_name = "price-test-real"
+
+
+
+        print("!: ", file_extension)
+        print("#: ", object_name)
+
+
+        bucket.upload_file(file_path, object_name, ExtraArgs={'ContentType': file_extension})
+        print(f'File uploaded successfully to {bucket_name}/{object_name}')
+
+        s3.Object(bucket_name, object_name).Acl().put(ACL='public-read')
+        print(f'File {object_name} is now public.')
+
+
+        # Get the URL of the uploaded file
+        file_url = f"https://{bucket_name}.s3.amazonaws.com/{object_name}"
+        print(f"File URL: {file_url}")
+
+        game["cover-link"] = file_url
+
+
+    except Exception as e:
+        print(e)
+
+
+
+
 #Initialize the DynamoDB client
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('price-analyzer-complete-2')
+table = dynamodb.Table('price-game-1')
 
 #plan : before putting things into database, if you want to use online storage, upload
 #the images in gamecoverlink to s3 bucket and update the link
@@ -37,51 +85,26 @@ for key, game in data.items():
 
 
 
-
-
-'''
-try:
-    response = table.scan()  # Retrieves all items from the table
-    items = response.get("Items", [])
-
-    # Print all items
-    if items:
-        for item in items:
-            print(item)
-    else:
-        print("No items found in the table.")
-except Exception as e:
-    print(f"Error retrieving items from the table. Error: {str(e)}")
-'''
-
-
-
-
-
+print("DONE")
 quit()
 
-#WIP for uploading files to the S3 bucket.
-
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('price-test-real')
-
-print(bucket)
 
 
-
-file_path = r"C:\Users\Jake\Desktop\Price Checker\Github-Repo\VideoGameAnalyzer\test.png"
-object_name = "bikething|pss1.png"
-bucket_name = "price-test-real"
+#file_path = r"C:\Users\Jake\Desktop\Price Checker\Github-Repo\VideoGameAnalyzer\test.png"
+#object_name = "bikething|pss1.png"
+#bucket_name = "price-test-real"
 # Upload the file to S3
 #it's not alwyays a png tho...
-bucket.upload_file(file_path, object_name, ExtraArgs={'ContentType': "png"})
-print(f'File uploaded successfully to {bucket_name}/{object_name}')
+#bucket.upload_file(file_path, object_name, ExtraArgs={'ContentType': "png"})
+#print(f'File uploaded successfully to {bucket_name}/{object_name}')
 
-s3.Object(bucket_name, object_name).Acl().put(ACL='public-read')
-print(f'File {object_name} is now public.')
+#s3.Object(bucket_name, object_name).Acl().put(ACL='public-read')
+#print(f'File {object_name} is now public.')
 
 
 # Get the URL of the uploaded file
-file_url = f"https://{bucket_name}.s3.amazonaws.com/{object_name}"
-print(f"File URL: {file_url}")
+#file_url = f"https://{bucket_name}.s3.amazonaws.com/{object_name}"
+#print(f"File URL: {file_url}")
+
+print("ok")
 
